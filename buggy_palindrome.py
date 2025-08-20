64a511827cfdc7d6f6ecc0570eaf9c03f5eea2f2
jj-devhub <Jaid.jashim@infinitibit.com>
2025-08-20 19:48:30 +0600

    Fix palindrome check to ignore case and spaces

--- buggy_palindrome.py
+++ buggy_palindrome.py
@@ -1,4 +1,5 @@
 def is_palindrome(s):
-    return s == s[::-1]  # Bug: does not ignore case or spaces
+    cleaned = ''.join(c.lower() for c in s if c.isalnum())
+    return cleaned == cleaned[::-1]
 
 print(is_palindrome("Race car"))
