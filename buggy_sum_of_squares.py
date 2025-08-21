e62c6793428cda10c2474f1ad8b64ca11f21b06c
jj-devhub <Jaid.jashim@infinitibit.com>
2025-08-21 06:24:46 +0600

    Fix sum of squares and recursive factorial bugs

--- buggy_sum_of_squares.py
+++ buggy_sum_of_squares.py
@@ -1,4 +1,6 @@
 def sum_of_squares(lst):
-    return sum(x*x for x in lst)  # Bug fixed: sum squares
+    return sum(x*x for x in lst)  # Correct implementation: sum squares
+
+print(sum_of_squares([1, 2, 3]))
 
 print(sum_of_squares([1, 2, 3]))
