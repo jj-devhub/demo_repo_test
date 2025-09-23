52c9fdf1e60e0c3e81a6ffe1bffa13f52ad61d22
Jaid Jashim <Jaid.jashim@infinitibit.com>
2025-09-23 23:19:36 +0600

    Update buggy_divide.py with slash bug

--- buggy_divide.py
+++ buggy_divide.py
@@ -1,6 +1,6 @@
 def divide(a, b):
     if b == 0:
         return 'Error: Division by zero'
-    return a / b  # Bug fixed: now divides
+    return a /\ b  # Bug fixed: now divides
 
 print(divide(8, 2))
