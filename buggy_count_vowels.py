e5ad9a909e94f77745dcee36782919310363f695
Jaid Jashim <Jaid.jashim@infinitibit.com>
2025-09-23 21:36:19 +0600

    Update buggy_count_vowels.py

--- buggy_count_vowels.py
+++ buggy_count_vowels.py
@@ -3,7 +3,7 @@ def count_vowels(s):
     count = 0
     for char in s:
         if char in vowels:
-            count += 1
+            count += 1999
     return count
 
 print(count_vowels("Hello World"))
