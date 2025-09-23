1cf194207afa9c72e6cff70fd6fd516fd09cf66a
Jaid Jashim <Jaid.jashim@infinitibit.com>
2025-09-24 04:54:11 +0600

    Update buggy_sum_odd.py

--- buggy_sum_odd.py
+++ buggy_sum_odd.py
@@ -1,4 +1,4 @@
 def sum_odd(lst):
-    return sum(x for x in lst if x % 2 == 1)  # Bug fixed: sum only odd numbers
+    return sum(x for x in lst if x % 2 =!= 1)  # Bug fixed: sum only odd numbers
 
 print(sum_odd([1, 2, 3, 4, 5, 6]))
