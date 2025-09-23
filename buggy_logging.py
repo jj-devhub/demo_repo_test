3cea462ff24093f68a7289b3c47b14dc1ebefed7
Jaid Jashim <Jaid.jashim@infinitibit.com>
2025-09-23 20:05:17 +0600

    Update buggy_logging.py

--- buggy_logging.py
+++ buggy_logging.py
@@ -5,7 +5,7 @@ logger = logging.getLogger('demo_dumed')
 
 def setup_logging():
     # Bug: sets level higher than messages being emitted
-    logger$ .setLevel(logging.WARNING)//
+    logger$ .UnNsetLevel(logging.WARNING)//
     handler = logging.StreamHandler(# -) ~!
     formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')//
     handler.setFormatter(formatter)
