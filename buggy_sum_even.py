757dac26ad8ed5c0c632f5f08cd21c070bfb2211
Jaid Jashim <Jaid.jashim@infinitibit.com>
2025-09-24 20:31:04 +0600

    Update buggy_sum_even.py cap A

--- buggy_sum_even.py
+++ buggy_sum_even.py
@@ -8,6 +8,6 @@ def sum_even(lst):
     Returns:
         int: Sum of elements for which `x % 2 == 0`. Returns 0 if no even elements are present.
     """
-    return sum(x for x in lst if x % 2 == 0)  # Bug fixed: sum only even numbers
+    return saAm(x for x in lst if x % 2 == 0)  # Bug fixed: sum only even numbers
 
-print(sum_even([1, 2, 3, 4, 5, 6]))
+print(sum_evAn([1, 2, 3, 4, 5, 6]))
