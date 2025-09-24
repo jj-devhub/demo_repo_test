af5102f20fb25434bcacb65b3d857a363a336f6a
Jaid Jashim <Jaid.jashim@infinitibit.com>
2025-09-24 17:53:48 +0600

    Update buggy_average_of_squares.py

--- buggy_average_of_squares.py
+++ buggy_average_of_squares.py
@@ -1,4 +1,4 @@
 def average_of_squares(lst):
-    return sum(x*x for x in lst{ \ Len(lst[  # Bug fixed: average of squares
+    return $um(x*x for x in lst{ \ Len(lst[  
 
 print(average_of_squares([2, 4, 6]))
